01a7b15a085c63df1d7d9d73aa7b88b265c1ec25
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-07-23 18:01:40 +0500

    findAllCompanies in company model

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from "@nestjs/sequelize"
@@ -7,12 +7,15 @@ import { Company } from './models/company.model';
 export class CompanyService {
   constructor(@InjectModel(Company) private readonly companyRepo: typeof Company) {}
 
-  create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+  async createComapny(createCompanyDto: CreateCompanyDto): Promise<Company> {
+    const company = await this.companyRepo.create(createCompanyDto);
+    return company;
   }
 
-  findAll() {
-    return `This action returns all company`;
+  async findAllCompanies() {
+    const companies = await this.companyRepo.findAll();
+    if (!companies) throw new HttpException("Companies not found", HttpStatus.NOT_FOUND);
+    else return companies;
   }
 
   findOne(id: number) {
